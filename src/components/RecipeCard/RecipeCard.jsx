9f41f7408661bf3f4ac0a70fd2f58149404fa508
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-23 13:14:28 +0300

    commit second

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -17,6 +17,21 @@ import {
 } from "./RecipeCard.styles";
 import { RecipeDifficulty } from 'constants';
 
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 export const RecipeCard = ({ item: { id, image, name, time, servings, calories, difficulty }, onDelete, onSelect }) => {    
     return (
         <div>
@@ -55,6 +70,9 @@ export const RecipeCard = ({ item: { id, image, name, time, servings, calories, 
                     <MdZoomIn size={20} />
                 </button>
             </div>
+
+           
+
         </div>);
 };
  
